a74cfe87323d799ba317b64e70ec4466dbae7f6a
soulcodingmatt <matt@soulcodingmatt.com>
2025-06-06 23:38:43 +0200

    feature: add basic annotations

---
+++ src/main/java/org/soulcodingmatt/equilibrium/annotations/GenerateDto.java
@@ -0,0 +1,29 @@
+package org.soulcodingmatt.equilibrium.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to generate a DTO class from the annotated class.
+ * The generated class will mirror the structure of the annotated class,
+ * excluding any fields marked with @IgnoreDto or @IgnoreAll.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface GenerateDto {
+    /**
+     * The package where the DTO should be generated.
+     * If not specified, the global package configuration from pom.xml will be used.
+     * @return the target package name
+     */
+    String packageName() default "";
+
+    /**
+     * The postfix to be added to the generated class name.
+     * Default is "Dto".
+     * @return the postfix for the generated class
+     */
+    String postfix() default "Dto";
+} 
