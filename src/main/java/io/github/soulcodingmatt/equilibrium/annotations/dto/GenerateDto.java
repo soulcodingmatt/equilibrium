4afdb6cd05ea0ef1371f2e8b212e0f62da07f2be
soulcodingmatt <matt@soulcodingmatt.com>
2025-06-15 18:24:18 +0200

    chore: prepare v0.1.0-RC1

---
+++ src/main/java/io/github/soulcodingmatt/equilibrium/annotations/dto/GenerateDto.java
@@ -0,0 +1,37 @@
+package io.github.soulcodingmatt.equilibrium.annotations.dto;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to generate a DTO class from the annotated class.
+ * The generated class will mirror the structure of the annotated class,
+ * excluding any fields marked with @IgnoreDto or @IgnoreAll.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface GenerateDto {
+    /**
+     * The package where the DTO should be generated.
+     * If not specified, the global package configuration from pom.xml will be used.
+     * @return the target package name
+     */
+    String pkg() default "";
+
+    /**
+     * The postfix to be added to the generated class name.
+     * Default is "Dto".
+     * @return the postfix for the generated class
+     */
+    String postfix() default "Dto";
+
+    /**
+     * Flag whether the generated DTO should have Lombok's @SuperBuilder annotation.
+     * Use builder = true, if you want to use the Builder Pattern with the generated DTOs.
+     * Default is false.
+     * @return
+     */
+    boolean builder() default false;
+} 
---
+++ src/main/java/io/github/soulcodingmatt/equilibrium/annotations/record/IgnoreRecord.java
@@ -0,0 +1,15 @@
+package io.github.soulcodingmatt.equilibrium.annotations.record;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a field to be ignored when generating the corresponding Record class.
+ * Fields annotated with this annotation will not be included in the generated Record.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface IgnoreRecord {
+}
