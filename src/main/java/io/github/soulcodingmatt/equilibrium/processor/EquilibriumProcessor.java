30c9a3cc14b2d76f7a2501bb04c6b3916cdd818d
soulcodingmatt <matt@soulcodingmatt.com>
2025-06-19 22:11:12 +0200

    chore: remove unused imports

--- src/main/java/io/github/soulcodingmatt/equilibrium/processor/EquilibriumProcessor.java
+++ src/main/java/io/github/soulcodingmatt/equilibrium/processor/EquilibriumProcessor.java
@@ -23,8 +23,6 @@ import javax.lang.model.element.VariableElement;
 import javax.tools.Diagnostic;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 @AutoService(Processor.class)
