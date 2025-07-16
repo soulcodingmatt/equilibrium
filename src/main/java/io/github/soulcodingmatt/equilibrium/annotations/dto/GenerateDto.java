2404097ae7c981f059b9ec5929019659fc04cbbb
soulcodingmatt <matt@soulcodingmatt.com>
2025-07-16 19:18:26 +0200

    refactor: replace argument "postfix" with "name" in the DTO generation, adjust logic to define complete file names

--- src/main/java/io/github/soulcodingmatt/equilibrium/annotations/dto/GenerateDto.java
+++ src/main/java/io/github/soulcodingmatt/equilibrium/annotations/dto/GenerateDto.java
@@ -34,11 +34,13 @@ public @interface GenerateDto {
     String pkg() default "";
 
     /**
-     * The postfix to be added to the generated class name.
-     * Default is "Dto".
-     * @return the postfix for the generated class
+     * The complete name for the generated DTO class.
+     * If specified, this exact name will be used without any postfix.
+     * If not specified, the class name will be constructed using the original class name 
+     * plus a postfix (determined by compiler arguments or the default "Dto").
+     * @return the complete name for the generated class
      */
-    String postfix() default "Dto";
+    String name() default "";
 
     /**
      * The fields of the base class to ignore. They will not be generated in the DTO.
